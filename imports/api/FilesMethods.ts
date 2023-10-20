2ab74888d13050a36f63a2e89800ec458a9472cb
Nicolas Mondain <nicolas@sharingbox.fr>
2023-10-20 14:35:47 +0200

    SimpleSchema

--- imports/api/FilesMethods.ts
+++ imports/api/FilesMethods.ts
@@ -1,18 +1,31 @@
 import {LemtestFile} from '../../@types/lemtest';
 import {Meteor} from 'meteor/meteor';
 import {FilesCollection} from './FilesCollection';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.methods({
 
 	'files.increment.downloaded'(_id: LemtestFile['_id']){
 
-		if(!_id){
+		try{
 
-			throw new Meteor.Error('_id is required');
+			const validation = new SimpleSchema({_id: String}).newContext();
+
+			validation.validate({_id});
+
+			if(!_id){
+
+				throw new Meteor.Error('files.increment.downloaded.MISSING_PARAM');
+
+			}
+
+			return FilesCollection.update(_id as string, {$inc: {downloaded: 1}});
+
+		}catch(error: any){
+
+			throw new Meteor.Error(JSON.stringify(error));
 
 		}
-
-		return FilesCollection.update(_id as string, {$inc: {downloaded: 1}});
 
 	}
 
